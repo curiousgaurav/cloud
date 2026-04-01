f62a18a00d88afa7fea7af89435f6b9a76d6392d
curiousgaurav <161939641+curiousgaurav@users.noreply.github.com>
2026-04-01 13:36:13 +0000

    cicd testing

--- task-3-expose-container/app.js
+++ task-3-expose-container/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
-// Middleware
+// Middleware new 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
